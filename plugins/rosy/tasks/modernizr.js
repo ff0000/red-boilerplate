7e704b37c050f96a56c0abf93149faa6e1ab68d2
Richard Herrera <rich@doctyper.com>
2013-07-03 16:13:25 -0700

    Upgrade grunt-modernizr to use Modernizr 3.0

--- plugins/rosy/tasks/modernizr.js
+++ plugins/rosy/tasks/modernizr.js
@@ -11,57 +11,43 @@ module.exports = function (grunt) {
 
 	// Project configuration.
 	grunt.config.set("modernizr", {
+		"dist": {
+			// [REQUIRED] Path to the build you're using for development.
+			"devFile" : path.join(source, "libs", "modernizr", "index.js"),
 
-		// [REQUIRED] Path to the build you're using for development.
-		"devFile" : path.join(source, "libs", "modernizr", "index.js"),
+			// [REQUIRED] Path to save out the built file.
+			"dest" : path.join(output, "libs", "modernizr", "modernizr.min.js"),
 
-		// [REQUIRED] Path to save out the built file.
-		"outputFile" : path.join(output, "libs", "modernizr", "modernizr.min.js"),
+			// Based on default settings on http://modernizr.com/download/
+			"options" : [
+				"setClasses",
+				"addTest",
+				"html5printshiv",
+				// "load",
+				"testProp",
+				"fnBind"
+			],
 
-		// Based on default settings on http://modernizr.com/download/
-		"extra" : {
-			"shiv" : true,
-			"printshiv" : false,
-			"load" : true,
-			"mq" : false,
-			"cssclasses" : true
-		},
+			// By default, source is uglified before saving
+			"uglify" : true,
 
-		// Based on default settings on http://modernizr.com/download/
-		"extensibility" : {
-			"addtest" : false,
-			"prefixed" : false,
-			"teststyles" : false,
-			"testprops" : false,
-			"testallprops" : false,
-			"hasevents" : false,
-			"prefixes" : false,
-			"domprefixes" : false
-		},
+			// Define any tests you want to impliticly include.
+			"tests" : [],
 
-		// By default, source is uglified before saving
-		"uglify" : true,
+			// By default, this task will crawl your project for references to Modernizr tests.
+			// Set to false to disable.
+			"crawl" : true,
 
-		// Define any tests you want to impliticly include.
-		"tests" : [],
+			// When parseFiles = true, this task will crawl all *.js, *.css, *.scss files.
+			// You can override this by defining a "files" array below.
+			"files" : [
+				path.join(source, "**", "*.js"),
+				path.join(source, "..", "{sass,scss}", "**", "*.scss")
+			],
 
-		// By default, this task will crawl your project for references to Modernizr tests.
-		// Set to false to disable.
-		"parseFiles" : true,
-
-		// When parseFiles = true, this task will crawl all *.js, *.css, *.scss files.
-		// You can override this by defining a "files" array below.
-		"files" : [
-			path.join(source, "**", "*.js"),
-			path.join(source, "..", "{sass,scss}", "**", "*.scss")
-		],
-
-		// When parseFiles = true, matchCommunityTests = true will attempt to
-		// match user-contributed tests.
-		"matchCommunityTests" : false,
-
-		// Have custom Modernizr tests? Add paths to their location here.
-		"customTests" : []
+			// Have custom Modernizr tests? Add paths to their location here.
+			"customTests" : []
+		}
 	});
 
 	grunt.config.set("build.modernizr", ["modernizr"]);
